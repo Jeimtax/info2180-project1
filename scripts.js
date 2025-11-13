85db12b6bc19a76a59012b9ce1cc88c75d7e8682
jeim <159331654+Jeimtax@users.noreply.github.com>
2025-11-13 09:50:25 -0500

    Added a sprinkle of Javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', ()=>{
+    let form = document.querySelector('.newsletter form');
+    let message = document.querySelector('.message');
+
+    form.addEventListener('submit', (e) =>{
+        //prevents page reload leading to 405 error
+        e.preventDefault();
+        let email = document.getElementById('email').value;
+        if (email === "") {
+            message.textContent = "Please enter a valid email address";
+        }
+        else {
+            message.textContent = `Thank you! Your email address ${email} has been added to the mailing list`;
+        }
+})
+})
